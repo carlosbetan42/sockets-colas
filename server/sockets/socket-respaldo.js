8de03705e9810005c8ef8fcb89c93f7de6a67d45
Carlos Betancourt <carlos.betancourt@jalisco.gob.mx>
2020-07-27 10:42:33 -0500

    Primer commit Producción

---
+++ server/sockets/socket-respaldo.js
@@ -0,0 +1,37 @@
+const { io } = require('../server');
+const { TicketControl } = require('../classes/ticket-control');
+
+const ticketControl = new TicketControl();
+
+io.on("connection", (client) => {
+  console.log("Usuario conectado");
+
+  client.emit("enviarMensaje", {
+    usuario: 'Administrador',
+    mensaje: 'Bienvenido a esta aplicación'
+  });
+
+  client.on("disconnect", () => {
+    console.log("Usuario desconectado");
+  });
+
+  // Escuchar al cliente
+  client.on("enviarMensaje", (data, callback) => {
+    console.log(data);
+
+    // Solo le envía la info al que hizo la petición
+    client.emit("enviarMensaje", data);
+
+    // Envía la info a todos
+    client.broadcast.emit("enviarMensaje", data);
+    /*if (mensaje.usuario) {
+      callback({
+        respuesta: 'TODO SALIÓ BIEN'
+      });
+    } else {
+      callback({
+        respuesta: 'TODO SALIÓ MAL!!!!!!!!!!!'
+      });
+    }*/
+  });
+});
